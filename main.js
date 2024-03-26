a69e9472944b13999d332758b4a0b2ee19502efa
DESKTOP-U0S0QOD\khale <khaledabdelnasser411@gmail.com>
2024-03-26 05:49:15 +0200

    .

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
+  $(".info-icon").on("click", function () {
+    $(".popup").addClass("open-popup");
+    $(".close-overlay").addClass("open-popup");
+  });
 
-    $(".info-icon").on("click", function () {
-        $(".popup").addClass("open-popup");
-        $(".close-overlay").addClass("open-popup");
-    });
+  $(".close-overlay").on("click", function () {
+    $(".popup").removeClass("open-popup");
+    $(this).removeClass("open-popup");
+  });
 
-    $(".close-overlay").on("click", function () {
-        $(".popup").removeClass("open-popup");
-        $(this).removeClass("open-popup");
-    });
+  // Get Date Of Now And Print It In Html
+  var today = new Date();
+  var dd = today.getDate();
 
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  today = dd + "/" + mm + "/" + yyyy;
+
+  $(".date-of-now").text(today);
 });
